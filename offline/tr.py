c2bb2e9132d253a76080ee951a870262b77447f8
Семён <fedyrin4@gmail.com>
2025-01-18 18:31:23 +0900

    tr

---
+++ offline/tr.py
@@ -0,0 +1,6 @@
+user_input = input("Введите строку:")
+cleaned_input = ''.join(user_input.replace(" ", "")).lower()
+if cleaned_input == cleaned_input[::-1]:
+        print("Введенная строка является палиндромом.")
+else:
+        print("Введенная строка не является палиндромом.")
