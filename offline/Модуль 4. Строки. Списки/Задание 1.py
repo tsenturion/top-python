808d6e166e2832d97b554d8fec3fbdfe6487b40c
Sergey319 <sergej.shevelev.87@list.ru>
2025-01-25 03:05:26 +0300

    Шевелёв Сергей Олегович

---
+++ offline/Модуль 4. Строки. Списки/Задание 1.py
@@ -0,0 +1,27 @@
+#arithmetic_expression = input("Введите арифметическое выражение: ")
+from re import findall
+
+arithmetic_expression = "23+12"
+result = 0
+
+
+numbers = findall(r'\d+', arithmetic_expression)
+number1 = int(numbers[0])
+number2 = int(numbers[1])
+print(number1)
+print(number2)
+
+for i in arithmetic_expression:
+    if i == '+':
+        result = number1 + number2
+    elif i == '-':
+        result = number1 - number2
+    elif i == '*':
+        result = number1 * number2
+    elif i == '/':
+        result = number1 / number2
+print(result)
+
+
+
+
