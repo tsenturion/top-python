f4e809ab0e430275d853ad8e286034ed30ff5f21
Ovchinnikov A. S <leshaovchinnikov1133@gmail.com>
2025-05-30 19:29:45 +0500

    zadanie1

---
+++ uyty/zadanie1c.py
@@ -0,0 +1,24 @@
+import socket
+
+HOST = '127.0.0.1'
+PORT = 5000
+
+def start_client():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_sock:
+        client_sock.connect((HOST, PORT))
+        print(f"Подключено к серверу {HOST}:{PORT}")
+
+        while True:
+            message = input("Клиент: ")
+            client_sock.sendall(message.encode())
+            if message.lower() == 'exit':
+                print("Завершение работы клиента.")
+                break
+            data = client_sock.recv(1024).decode()
+            print(f"Сервер: {data}")
+            if data.lower() == 'exit':
+                print("Сервер завершил соединение.")
+                break
+
+if __name__ == "__main__":
+    start_client()
