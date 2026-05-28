d1344af8b089e5fd014b9f3d605c35a737b604a2
Ефимов Матвей Александрович <80580892+tsenturion@users.noreply.github.com>
2026-05-28 21:02:39 +0500

    os

--- 1600_Системное_программирование/1601_os.py
+++ 1600_Системное_программирование/1601_os.py
@@ -2,3 +2,26 @@
 1601_os
 """
 
+import os
+
+current_directory = os.getcwd()
+print(current_directory)
+
+os.chdir('..')
+print(os.getcwd())
+
+files = os.listdir()
+print(files)
+
+files = os.listdir('..')
+print(files)
+
+print(os.path.isfile('1601_os.py'))
+print(os.path.isdir('1601_os.py'))
+print(os.path.exists('1601_os.py'))
+
+path = 'folder/file.txt'
+
+path = os.path.join('folder', 'file.txt')
+print(path)
+
