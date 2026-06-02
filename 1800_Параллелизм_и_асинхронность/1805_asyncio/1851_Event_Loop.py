876e03fe1c32c0a2fa902cf26879f0e74a61a824
Ефимов Матвей Александрович <80580892+tsenturion@users.noreply.github.com>
2026-06-02 22:42:49 +0500

    asyncio

--- 1800_Параллелизм_и_асинхронность/1805_asyncio/1851_Event_Loop.py
+++ 1800_Параллелизм_и_асинхронность/1805_asyncio/1851_Event_Loop.py
@@ -2,3 +2,20 @@
 1851_Event_Loop
 """
 
+"""
+response1 = request()
+response2 = request()
+response3 = request()
+"""
+
+import asyncio
+
+async def request(number):
+    print(f'request {number}')
+    await asyncio.sleep(1)
+    print(f'response {number}')
+    
+async def main():
+    await asyncio.gather(request(1), request(2), request(3))
+
+asyncio.run(main())
