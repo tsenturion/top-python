619b1edbc8e43d4eea6a127ba9693ad4771ede76
Ефимов Матвей Александрович <80580892+tsenturion@users.noreply.github.com>
2026-06-02 22:04:15 +0500

    t2

--- 1800_Параллелизм_и_асинхронность/1803_threading/1831_Thread.py
+++ 1800_Параллелизм_и_асинхронность/1803_threading/1831_Thread.py
@@ -9,8 +9,42 @@ def worker():
     print("Поток закончил работу")
     
 
-worker()
+#worker()
 thread = threading.Thread(target=worker)
 thread.start()
 thread.join()
+print("Главный поток завершен")
+
+
+
+print()
+def worker(name):
+    print(f"Поток {name} начал работу")
+    print(f"Поток {name} закончил работу")
+
+thread = threading.Thread(target=worker, args=("Thread-1",))
+thread.start()
+thread.join()
+print("Главный поток завершен")
+
+
+print()
+import time
+
+def worker(number):
+    print(f"Поток {number} начал работу")
+    time.sleep(1)
+    print(f"Поток {number} закончил работу")
+    
+    
+threads = []
+
+for i in range(5):
+    thread = threading.Thread(target=worker, args=(i,))
+    threads.append(thread)
+    thread.start()
+
+for thread in threads:
+    thread.join()
+
 print("Главный поток завершен")
